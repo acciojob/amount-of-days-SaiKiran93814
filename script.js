7d910e056763f865d4b3d2923f65481d87251146
SaiKiran93814 <60767499+SaiKiran93814@users.noreply.github.com>
2025-04-19 16:07:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+function daysOfAYear(year) {
+  // Check for leap year
+  const isLeapYear = (year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0);
+  return isLeapYear ? 366 : 365;
+}
